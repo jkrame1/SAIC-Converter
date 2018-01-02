04cd942ca50785325f7d0fe929e0353e9f11cd0d
Joseph <jkrame1@saic.edu>
2018-01-02 14:20:48 -0600

    Updated for Debug, library begin() function, and scaling
    Betweener begin() function added to setup.
    Serial debug code added for testing.
    Funcitons that write CV values to DAC channels have been updated to provide full-scale values to the DAC.

--- Software/Experimentation/generativeSeq/generativeSeq.ino
+++ Software/Experimentation/generativeSeq/generativeSeq.ino
@@ -1,16 +1,16 @@
 /*
- * Generative Betweener Sequencer
- * 
- * requires Betweener libary
- * by Kyle Werle
- *  2017-12-31
- *  SAIC Art and Technology and Sound Departments
- *  Betweener Library by Kathryn Schaffer
- *  Hardware by Joseph Kramer
- */
+   Generative Betweener Sequencer
 
- /**SET LIBRARIES**/
- 
+   requires Betweener libary
+   by Kyle Werle
+    2017-12-31
+    SAIC Art and Technology and Sound Departments
+    Betweener Library by Kathryn Schaffer
+    Hardware by Joseph Kramer
+*/
+
+/**SET LIBRARIES**/
+
 #include <Betweener.h>
 #include <Wire.h>
 
@@ -28,12 +28,15 @@ int gateSeqL = 15;
 
 //  position of sequencer
 int CVseqCount = 0;
-int gateSeqCount= 0;
+int gateSeqCount = 0;
 
 /**SETUP**/
 
 void setup() {
-  
+  Serial.begin(115200);
+  b.begin();
+
+
   //  randomize seed based on ambient analog read
   randomSeed(analogRead(A17)*PI);
 
@@ -54,24 +57,30 @@ void loop() {
   clockCount();
 
   //  on trig 1 edge randomize CV sequencer
-  if (b.trig1.fallingEdge()){
+  if (b.trig1.fallingEdge()) {
+    Serial.println("trig 1");
     randomizeCVseq();
   }
 
   //  on trig 2 edge randomize gate sequencer
-  if (b.trig2.fallingEdge()){
+  if (b.trig2.fallingEdge()) {
+    Serial.println("trig 2");
     randomizeGateSeq();
+
   }
 
   //  on trig 3 edge set clock back to 0
-  if (b.trig3.fallingEdge()){
+  if (b.trig3.fallingEdge()) {
+    Serial.println("trig 3");
     CVseqCount = 0;
     gateSeqCount = 0;
   }
 
   //  write CV and gate to DAC1 and DAC2
   b.writeCVOut(CVsequence[CVseqCount], 1);
+  //Serial.println(CVsequence[CVseqCount]);
   b.writeCVOut(gateSequence[gateSeqCount], 2);
+  //Serial.println(gateSequence[gateSeqCount]);
 
 }
 
@@ -79,16 +88,18 @@ void loop() {
 
 //  step clock count forward on rising trigger edge
 void clockCount() {
-  
-  if (b.trig4.fallingEdge()){
-    
-    if (CVseqCount < CVseqL){
+
+  if (b.trig4.fallingEdge()) {
+    Serial.println("trig 4");
+    if (CVseqCount < CVseqL) {
       CVseqCount++;
     } else {
       CVseqCount = 0;
     }
 
-    if (gateSeqCount < gateSeqL){
+    // Serial.println(CVseqCount);
+
+    if (gateSeqCount < gateSeqL) {
       gateSeqCount++;
     } else {
       gateSeqCount = 0;
@@ -98,37 +109,40 @@ void clockCount() {
 
 //  randomizes CV sequencer
 //  knob positions control output possibilities
-void randomizeCVseq(){
-  
-  int CV0 = b.CVtoMIDI(b.currentKnob1);
-  int CV1 = b.CVtoMIDI(b.currentKnob2);
-  int CV2 = b.CVtoMIDI(b.currentKnob3);
-  int CV3 = b.CVtoMIDI(b.currentKnob4);
-  
-  for (int i = 0; i <= 15; i++){
+void randomizeCVseq() {
+
+  int CV0 = b.readKnobCV(1);
+  int CV1 = b.readKnobCV(2);
+  int CV2 = b.readKnobCV(3);
+  int CV3 = b.readKnobCV(4);
+
+  for (int i = 0; i <= 15; i++) {
     int sel;
     int r = random(4);
 
-    if (r == 0){
+    if (r == 0) {
       sel = CV0;
-    } else if (r == 1){
+    } else if (r == 1) {
       sel = CV1;
-    } else if (r == 2){
+    } else if (r == 2) {
       sel = CV2;
-    } else if (r == 3){
+    } else if (r == 3) {
       sel = CV3;
     }
-    
+
     CVsequence[i] = sel;
   }
 }
 
 //  randomize gate sequencer
 //  UPDATE TO INCLUDE DENSITY
-void randomizeGateSeq(){
+void randomizeGateSeq() {
 
-  for (int i = 0; i <= 15; i++){
+  for (int i = 0; i <= 15; i++) {
     float rF = random(2);
+    if (rF == 1) {
+      rF = 4095;
+    }
     int rI = (int) rF;
 
     gateSequence[i] = rI;
