c45935a38543cca64cdf176be8aef908484dffcc
kathryn schaffer <kathrynschaffer@kathryns-MacBook-Air-2.local>
2018-01-16 22:00:39 -0600

    new constructor, many new functions to modify playback

--- Software/Pattern/Pattern.cpp
+++ Software/Pattern/Pattern.cpp
@@ -1,135 +1,201 @@
 //
 //  Pattern.cpp
-//  
-//
-//  Created by kathryn schaffer on 12/29/17.
-//
 //
 
 #include "Pattern.h"
 
 Pattern::Pattern(float length_in_sec, int n_segments, int millisec_time_step){
- 
-    //note to self... code hangs if you ask for more segments than will fit given
-    //time and dt values.  Need a check and correction for that condition
+    //This is the constructor function that builds the empty pattern.
+    //if user inputs are nonsensical, it corrects them.
+    //Fills pattern with zeros to start, and parses out segment start/stop times
     
-    length_ = length_in_sec;
+    length_ = (float)length_in_sec;
+ 
     lengthmsec_ = length_ * 1000.;
     nseg_ = n_segments;
     dt_ = millisec_time_step;
 
+    //test and rationalize the inputs.  This may result in modifying what the user requested.
     
-    //test and rationalize the inputs - that is, ensure that length is an
-    //integer multiple of the dt value. If it isn't, we make different decisions if
-    //the DTPRIORITY flag is set or not.
-
     float remainder = fmod(lengthmsec_, dt_); //fmod is defined in math.h; it calculates remainder
     nsamp_ = (int) (lengthmsec_ / dt_); //calculates integer number of dt samples that will fit
 
-    //if the number of samples requested exceeds the maximum table length, we have a
-    //problem.  Issue an error message and truncate the pattern so that it will fit.
-    
-    if (nsamp_ > TABLELENGTH){
-        DEBUG_PRINT("Requested pattern exceeds the available array length for the table.");
-        DEBUG_PRINT("Modifying the pattern to fit.");
-        
-        nsamp_ = TABLELENGTH;
-#ifdef DTPRIORITY
-
-        lengthmsec_ = nsamp_ * dt_;
-        length_ = lengthmsec_ * 1000.;
-#else
-        dt_ = lengthmsec_ / nsamp_;
-#endif
-        
-    }  else {
-        
-        //Even if the number of samples fits, we could have a problem that there's not an
-        //integer number of time samples in the requested length.  We will correct this in
-        //different ways depending on the DTPRIORITY flag
-    
 #ifdef DTPRIORITY
         //in this case, we are taking the dt value as the most important time unit
         //and rationalizing to that
         //so if the specified time is longer than the nearest integer number of dt samples, we reset it
-        if (remainder > 0.){
-            lengthmsec_ = nsamp_ * dt_;
-            length_ = lengthmsec_ / 1000.;
-            DEBUG_PRINT("adjusting the total length of your pattern to be an integer number of samples");
-        }
+    if (remainder > 0.){
+        lengthmsec_ = nsamp_ * dt_;
+        length_ = lengthmsec_ / 1000.;
+        //    DEBUG_PRINT("adjusting the total length of your pattern to be an integer number of samples");
+    }
 #else
         //in this case, we are taking the length of the pattern as the most important time unit
         //and rationalizing to that.  Thus if the dt is off, we reset it.
-        if (remainder > 0.){
-            dt_ = lengthmsec_ / nsamp_;
-            DEBUG_PRINT("adjusting the time step of your pattern to be an integer divisor of the length");
-        }
-#endif
+    if (remainder > 0.){
+        dt_ = lengthmsec_ / nsamp_;
+       //     DEBUG_PRINT("adjusting the time step of your pattern to be an integer divisor of the length");
     }
+#endif
+
+  //   equal_ = true;
+
     
     float timeperseg = lengthmsec_ / nseg_;
     
-    //check that number of segments is not too many
-    if (nseg_ > MAXNSEG){
-        DEBUG_PRINT("you requested more than MAXNSEG segments.  Truncating to MAXNSEG.");
-        nseg_ = MAXNSEG;
-    }
-    //check that the number of segments is not too few (need at least one time sample per segment)
-    if (timeperseg < dt_){
-        DEBUG_PRINT("you requested more segments than time points.  Making each time point a segment.");
-        nseg_ = nsamp_;
-    }
-
-    //fill in arrays of start/stop indexes for each segment
-
-    int n = 0; //index of current segment
-    float currenttime = 0.;
-    bool newseg = true;
+    
     // we loop through all samples and assign start/stop indexes to the segments.
     // the reason for doing it this way is so that the number of segments truly divides
     // the total time, not requiring that a perfectly even number of samples
     // be in each segment
+    int n = 0; //index of current segment
+    float currenttime = 0.;
+    
+    //initialize the first segment
     segstarts_[0] = 0;
     segends_[0] = 0;
+    whichSeg_[0]=0;
     for (int k=0; k<nsamp_; k++){
         currenttime = k*dt_;
-//        if (newseg == true){
-//           // segstarts_[n] = k;
-//            segstarts_[n] = k;
-//            segends_[n] = k;
-//            newseg = false;
-//        }else{
-            if (currenttime < ((n+1) * timeperseg)){
-                //segment end index gets overridden until
-                // we reach the 'else' condition
-                segends_[n] = k;
-            }else{
-                if (n+1 < nseg_){
-                    segstarts_[n+1] = k;
-                    segends_[n+1] = k;
-                }
-                newseg = true;
-                n = n+1;
+        if (currenttime < ((n+1) * timeperseg)){
+            //segment end index gets overridden until
+            // we reach the 'else' condition
+            segends_[n] = k;
+            //  whichseg_[k]=n;
+        }else{
+            
+            if (n+1 < nseg_){
+                segstarts_[n+1] = k;
+                segends_[n+1] = k;
             }
-//        }
+            
+            n = n+1;
+            
+        }
+        whichSeg_[k]=n;
     }
     //final end point:
     segends_[nseg_ - 1] = nsamp_ - 1;
     
     
-    //initialize table to all zeros
-    for (int k = 0; k < nsamp_; k++){
-        table[k]=0.;
+    
+    //initialize all the variables
+    init();
+    
+
+}
+
+
+Pattern::Pattern(int n_segments, int millisec_time_step, int* segment_lengths_ms){
+    //this constructor lets you set time windows for the segments so they can be uneven.
+    //these are set in millisec
+    
+    //maybe add a check for stupid stuff (negative inputs etc.)?
+    
+    // get it all set up
+    dt_ = millisec_time_step;
+    nseg_ = n_segments;
+
+    int total=0;
+    for (int k=0;k<nseg_; k++){
+        total+=segment_lengths_ms[k];
     }
-  
+    lengthmsec_ = (float)total;
+    length_ = lengthmsec_/1000.;
+ //   Serial.println(String("length ")+length_);
+    nsamp_ = (int)(total/dt_);
+
     
-    //initialize index variable to 0
-    isamp_ = 0;
+ //   equal_ = false;
+
+    for (int k=0; k<nseg_; k++){
+        seglengths_[k]=segment_lengths_ms[k];
+    }
     
-    //initialize to a non-playing state (we are not tracking time)
-    isPlaying_ = false;
+    
+    
+    int i=0; //current seg
+    int t=0;  //t in ms
+    int accumulated_length = 0;
+    segstarts_[0]=0;
+    segends_[0] = 0;
+    
+    for (int k=0; k<nsamp_; k++){
+        
+        t=k*dt_;
+        if (t < accumulated_length+seglengths_[i]){
+            
+            segends_[i]=k;
+            whichSeg_[k]=i;
+            
+        }else{
+            
+            accumulated_length = accumulated_length+seglengths_[i];
+            
+            //increment up and start working with the next segment
+            i=i+1;
+            segstarts_[i]=k;
+            segends_[i]=k;
+            whichSeg_[k]=i;
+            
+        }
+        
+    }
+
+    
+    init();
     
 }
+
+
+//Pattern::~Pattern(){
+  //  free(table_);
+  //  free(segstarts_);
+  //  free(segends_);
+  //  free(doRandom_);
+  //  free(segTscale_);
+  //  free(whichSeg_);
+//}
+
+
+void Pattern::init(){
+    
+    //all lines below are initializing state and data variables to
+    //default settings, many of which will get overridden through function calls
+    for (int k = 0; k < nsamp_; k++){
+        table_[k]=0.;
+        
+    }
+    
+    for (int k=0; k<nseg_; k++){
+        doRandom_[k]=false;
+    }
+    odt_ = dt_;
+   
+    randMin_ = 0;
+    randMax_ = 127;
+    isamp_ = 0;
+    isPaused_ = false;
+    isPlaying_ = false;
+    lastVal_ = 0;
+    lastTime_ = 0;
+    tScale_ = 1.;
+    forward_ = true;
+    for (int k=0; k<nseg_; k++){
+        segTscale_[k] = 1.0;
+    }
+    done_ = false;
+
+
+
+}
+
+
+void Pattern::setRandMinMax(int min, int max){
+    randMin_ = min;
+    randMax_ = max;
+}
+
 
 void Pattern::assignConstToSegment(int segment_index, float val){
     
@@ -138,7 +204,7 @@ void Pattern::assignConstToSegment(int segment_index, float val){
     }else{
         int n = segends_[segment_index] - segstarts_[segment_index];
         for (int i = 0; i<n+1; i++){
-            table[segstarts_[segment_index] + i] = val;
+            table_[segstarts_[segment_index] + i] = val;
         }
     }
     
@@ -152,7 +218,7 @@ void Pattern::assignConstToSegments(int segment_index_start, int segment_index_e
 }
 
 void Pattern::assignRampToSegment(int segment_index, float startval, float endval){
-    
+
     if (segment_index < 0 or segment_index > nseg_){
         DEBUG_PRINT("you are trying to modify a nonexistent segment.  Ignoring your request.");
                                                                                 
@@ -161,7 +227,7 @@ void Pattern::assignRampToSegment(int segment_index, float startval, float endva
         float stepsize =  (endval - startval)/n;
         int begin_index=segstarts_[segment_index];
         for (int i = 0; i<n+1; i++){
-            table[begin_index + i] = startval + i*stepsize;
+            table_[begin_index + i] = startval + i*stepsize;
         }
         
     
@@ -175,6 +241,16 @@ void Pattern::assignRampToSegments(int segment_index_start, int segment_index_en
         assignRampToSegment(i, startval, endval);
     }
 }
+
+
+
+
+void Pattern::assignRandToSegment(int segment_index){
+    if (segment_index > 0 && segment_index <= nseg_){
+        doRandom_[segment_index] = true;
+    }
+}
+
 
 void Pattern::fillSteps(float* step_sequence){
     for (int i = 0; i<nseg_; i++){
@@ -192,69 +268,218 @@ void Pattern::fillRamps(float* ramp_endpoints){
 }
 
 
+void Pattern::fillFrom(float* list_of_values){
+    //bad things will happen if your list is not long enough, beware!
+    //here we just presume it is
+    
+    for (int i =0; i<nsamp_; i++){
+        table_[i]=list_of_values[i];
+        
+    }
+}
+
+void Pattern::fillSegmentFrom(int segment_index, float* list_of_values){
+    //we presume the list is long enough
+    int k=0;
+    for (int i=segstarts_[segment_index]; i<= segends_[segment_index]; i++){
+        table_[i]=list_of_values[k];
+        k=k+1;
+    }
+}
 
 void Pattern::rescaleTo(float minval, float maxval){
     float currentmax = getMax();
     float currentmin = getMin();
     float newval;
     for (int i =0; i<nsamp_; i++){
-        newval=fMap(table[i],currentmin, currentmax, minval, maxval);
-        table[i]=newval;
+        newval=fMap(table_[i],currentmin, currentmax, minval, maxval);
+        table_[i]=newval;
     }
 }
 
-
+bool Pattern::done(){
+    //there are conditions under which this will fail... if the
+    //amount of time taken for the sketch loop to run is longer thna dt_,
+    //we will not catch the short-term flag that shows we just looped
+    //around a full cycle.  But otherwise, it should work
+    return done_;
+}
 
 float Pattern::next(){
+  
+   
+    //this is the main playback function.  Mostly, it pulls values from the
+    //internal table and returns them in order with the right timing
+    //but it also is where random numbers and holds (pauses) are implemented
     int nextsamp;
-    if (!isPlaying_){
-        isPlaying_ = true;
-        ticker_ = 0;
-        isamp_ = 0;
-    }else{
-        nextsamp = isamp_ + 1;
-        if (ticker_ > (nextsamp * dt_)){
-            if (nextsamp < nsamp_){
-                isamp_ = nextsamp;
+    float t;
+    float val;
+    bool newsamp = false;
+  //  float temp;
+    if (!isPaused_){
+      
+        if (!isPlaying_){
+            isPlaying_ = true;
+            ticker_ = 0;
+            isamp_ = 0;
+            iseg_ = 0;
+            done_ = false;
+            t=0;
+            lastTime_=0;
+            newsamp = true;
+          
+        }else{
+           
+            t = dt_ * tScale_ * segTscale_[iseg_];
+            if ( ticker_ > lastTime_ +  t){
+                lastTime_ = lastTime_+t;
+                newsamp = true;
+                if (forward_){
+                  //  Serial.println("moving forward");
+                    nextsamp = isamp_ + 1;
+                    if (nextsamp < nsamp_){
+                        isamp_ = nextsamp;
+                        done_ = false;
+                    //    Serial.println("incrementing isamp");
+                    } else {
+                        isamp_ = 0;
+                        done_ = true;
+                        ticker_=0;
+                        lastTime_=0;
+                  
+                    }
+                }else{
+                   // Serial.println("moving in reverse");
+                    nextsamp = isamp_ - 1;
+                    if (nextsamp > 0){
+                        isamp_ = nextsamp;
+                        done_=false;
+                   //     Serial.println("decrementing isamp");
+                    }else{
+                        isamp_ = nsamp_-1;
+                        done_=true;
+                        ticker_=0;
+                        lastTime_=0;
+                     
+                    }
+                }
+            
+                iseg_ = whichSeg_[isamp_];
             }else{
-                isamp_ = 0;
-                ticker_ = 0;
+                newsamp = false;
             }
+
+       
+       //     float temp=dt_*tScale_*segTscale_[iseg_];
+       //    Serial.println(String("ticker: ")+ticker_);
+       //     Serial.println(String("lastTime: ")+lastTime_);
+       //     Serial.println(String("dt*scales")+temp);
+       //     Serial.println(String("sample ")+isamp_+String(" seg ")+iseg_);
+      
         }
+       
+        if (doRandom_[iseg_] == true){
+            if (newsamp){
+                val = random(randMin_, randMax_);
+            }else{
+                val = lastVal_;
+            }
+        }else{
+            val = table_[isamp_];
+        }
+      //  Serial.println(val);
+        lastVal_ = val;
+  
+        return val;
+        
+    }else{
+        
+        return lastVal_;
     }
-    return table[isamp_];
+    
 }
 
+void Pattern::hold(){
+    if (!isPaused_){
+        isPaused_ = true;
+        pauseTicker_ = 0;
+        Serial.println("Pausing");
+    }
+}
 
+void Pattern::unHold(){
+    if (isPaused_){
+        isPaused_ = false;
+  //      Serial.println(String("ticker: ")+ticker_);
+
+        ticker_ = ticker_ - pauseTicker_;
+  //      Serial.println(String("ticker - pauseticker: ")+ticker_);
+    }
+}
 void Pattern::stop(){
     isPlaying_ = false;
 }
 
+void Pattern::reverse(){
+    forward_ = false;
+}
+
+void Pattern::forward(){
+    forward_ = true;
+}
+
+
+void Pattern::jumpToSeg(int segment_index){
+  //  Serial.println("Jumping!");
+    iseg_ = segment_index;
+    isamp_ = segstarts_[iseg_];
+}
+
+void Pattern::jumpTo(int sample_index){
+    isamp_ = sample_index;
+    iseg_ = whichSeg_[isamp_];
+
+}
+
+void Pattern::scaleTime(float scalefactor){
+  //  Serial.println(String("scaling: ")+scalefactor);
+    tScale_ = scalefactor;
+}
+void Pattern::scaleSegmentTime(int segment_index, float scalefactor){
+    segTscale_[segment_index]=scalefactor;
+}
+void Pattern::reset(){
+    tScale_=1.;
+    for (int k=0; k<nseg_; k++){
+        segTscale_[k]=1.0;
+    
+    }
+}
 void Pattern::printToSerial(){
     
     Serial.println(String("Pattern with length ")+length_+String(" seconds, ")+nseg_+String(" segments, and ")+dt_+String(" milliseconds between time samples."));
+//    Serial.println(String("length in millisec: ")+lengthmsec_);
     Serial.println(String("This pattern has a total of ")+nsamp_+String(" samples"));
+//    Serial.println(npersegment_);
     Serial.println("Here are the segment start/stop indices: ");
     for (int i = 0; i<nseg_; i++){
         Serial.println(String("segment: ")+i+String(" ")+segstarts_[i]+String(" ")+segends_[i]);
     }
     
-    
-    
- //   int n=0; // current segment index
+  
     for (int k = 0; k<nsamp_; k++){
         
-        Serial.println(String("index: ")+k+String(" pattern value: ")+table[k]);
+        Serial.println(String("index: ")+k+String(" pattern value: ")+table_[k]);
     }
                    
 }
 
 float Pattern::getMax(){
     float maxval;
-    maxval=table[0];
+    maxval=table_[0];
     for (int i=1; i<nsamp_; i++){
-        if (maxval < table[i]){
-            maxval = table[i];
+        if (maxval < table_[i]){
+            maxval = table_[i];
         }
     }
     return maxval;
@@ -262,10 +487,10 @@ float Pattern::getMax(){
 
 float Pattern::getMin(){
     float minval;
-    minval=table[0];
+    minval=table_[0];
     for (int i=1; i<nsamp_; i++){
-        if (minval > table[i]){
-            minval = table[i];
+        if (minval > table_[i]){
+            minval = table_[i];
         }
     }
     return minval;
--- Software/Pattern/Pattern.h
+++ Software/Pattern/Pattern.h
@@ -15,19 +15,26 @@
 //  Internally, it is stored as an array of floats.  Functions exist to
 //  rescale to a given range (e.g. -1 to 1 or 0 to 127).
 //
-//  Current implementation follows the common advice that dynamic memory
-//  allocation is best avoided for embedded code.  Teensy should be fine
-//  using dynamic memory allocation, but we'll play it safe by just
-//  pre-allocating the data arrays.  This will set a hard maximum on the
-//  number of points available in the pattern, controlled at compile time.
-//
+//  I tried dynamic memory allocation for the tables but it got
+//  buggy in odd ways.  For now, use the MAXNSAMP and MAXNSEG parameters
+//  here to pick reasonable sizes for your tables.
 //
 //  No effort has been made yet to optimize this for speed or memory.
 //  Also, right now it is totally unfinished and only partially tested.
 //
 //  Revision history:
 //  Jan 02 2018 first minimally functional version - KS
-
+//  Jan 14 2018 new constructor, playback disruptions, reverse, stretching, scaling
+//
+//  To do:
+//    -sine functions and ramp curves
+//    -callbacks
+//    -interpolation
+//    ** known bug:  if the processing speed is too fast, next() somehow does not keep up.
+//    for the time being, be sure to use some delay
+//    ** second known bug:  having problems if there is only one segment.  not sure what that is about.
+//    ** and another:  if your sketch delays more than 2 samples-worth, something weird happens
+//    fix the ambiguity between what happens with stop() and reset() to be something more logical
 
 
 #ifndef Pattern_h
@@ -36,12 +43,11 @@
 #include <math.h>
 #include <Arduino.h>
 
-// this sets the maximum number of entries in the table, which is
-// the maximum number of time points with individually computed function
-// values.
-#define TABLELENGTH 1024
+
 // maximum number of segments you can define
-#define MAXNSEG 32
+#define MAXNSEG 256
+// max table size (really didn't want to have to do this...)
+#define MAXNSAMP 1024
 
 // the variable below is a flag determining that if we have to make internal
 // adjustments to 'rationalize' the pattern, the delta-t is taken as
@@ -49,9 +55,8 @@
 // if you comment it out, then length is taken as the priority.  
 #define DTPRIORITY
 
-//If you want the code to give you some error messages in the
-//serial monitor, leave the line below. Otherwise, comment it out.
-#define DEBUG  //leave this line in to have automatic error printouts to Serial monitor
+
+#define DEBUG  //leave this line in to have automatic error printouts to Serial monitor; comment out otherwise
 //this little "preprocessor macro" (which is like a function but processed at a different time)
 //will be used to print the debug info.  See examples in the .cpp file for how this gets used.
 #ifdef DEBUG
@@ -69,28 +74,33 @@ class Pattern
 public:
     
     Pattern(float length_in_sec, int n_segments, int millisec_time_step);
+    Pattern(int n_segments, int millisec_time_step, int* segment_lengths_ms); //segment_time_bounds needs
+                                                                            //n_segments elements
+  //  ~Pattern();
+    
     
     //***************
     // Functions to make the pattern:
     //***************
     void fillSteps(float* step_sequence);  //step_sequence must have n_segments elements
     void fillRamps(float* ramp_endpoints); //ramp_endpoints must have n_segments+1 elements
+    void fillFrom(float* list_of_values); //list of values must have enough!
+    void fillSegmentFrom(int segment_index, float* list_of_values);  //list of values must have enough!
     
     void assignConstToSegment(int segment_index, float val);
     void assignConstToSegments(int segment_index_start, int segment_index_end, float val);
     void assignRampToSegment(int segment_index, float startval, float endval);
     void assignRampToSegments(int segment_index_start, int segment_index_end, float startval, float
                                endval);
-    void assignRandomIntsToSegment(int segment_index, int min, int max);
-    void assignRandomIntsToSegment(int segment_index_start, int segment_index_stop, int min, int max);
+ 
+    void setRandMinMax(int min, int max); //min and max parameters for random number generator
+                                          //defaults to 0 to 127
+    //use integer random numbers with min and max defined by the setRandMinMax function to
+    //fill specified segments any time they are playing:
+    void assignRandToSegment(int segment_index);
+    void assignRandToSegments(int segment_index_start, int segment_index_stop);
 
     
-    //placeholders for brainstorming some of the other things I might want to add
-//    void assignHoldToSegment(int segmnet_index); // hold last value of previous segment
-//    void assignHoldToSegments(int segment_index_start, int segment_index_end);
-    //void assignExpUpToSegment(int segment_index, ...
-    //void assignExpDownToSegment(...
-    //
 
     //callback functions for assigning functions to segments
     //the index i is the index of the time step from the start of the pattern;
@@ -106,13 +116,13 @@ public:
     //****************
     //Functions to modify the pattern
     //****************
+    //rescales whole pattern.  NOTE:  does not rescale any random segments.  change those with
+    //the setRandMinMax function.
     void rescaleTo(float minval, float maxval);
-    //internally, the table is always stored as floats.  what quantizeTo does is *round* to
-    //a set of steps in the specified range, rounding up or down to the nearest interval.
-    //this will give a different behavior than casting to int, which just truncates.
-    //also, it lets you do things like quantize to 3 steps within a range from 0 to 127
-    //if for some reason you might want to do that
- //   void quantizeTo(int nsteps, int min, int max);   //not implemented yet
+ 
+    //this is a placeholder... I think actually that quantizing should be left to the
+    //pattern bank code and not put here.
+//    void quantizeTo(int nsteps, int min, int max);   //not implemented yet
     
     
     
@@ -121,12 +131,29 @@ public:
     //Functions related to playing/displaying the pattern
     //***************
 
-    float next();  //main function for playback.  returns new values from table only when
-                   //enough time has elapsed; otherwise, returns same current value
-//    float nextWait();  //this version takes over the main loop until the next time unit change
+    float next();  //main function for playback in a loop.
     
+    float once();  //main function for once-through playback
+    
+    void hold();   //continues returning the same value until unHold is called
+    void unHold();
+
     
     void stop();  //stops playback (just sets an internal flag that makes things reset at next next() call)
+    
+    void reverse(); //as soon as this is called, begins playing pattern backwards
+    void forward();  //returns to forward progression
+    
+    // stretch or compress the time intervals
+    void scaleTime(float scalefactor);  //for whole pattern
+    void scaleSegmentTime(int segment_index, float scalefactor);  //for just one segment
+    //jump around
+    void jumpToSeg(int segment_index);
+    void jumpTo(int sample_index);
+    
+    void reset(); //restores original time
+    
+    bool done(); //returns a flag for whether the pattern has gone through one complete cycle
     
     //reassuring printout
     void printToSerial(void);
@@ -142,24 +169,50 @@ public:
     
     
 private:
+    
+    
+    void init(); //private initialization function.  Called by both constructors.  Sets defaults.
+    
+    
     //using the convention that an added underscore at the end of each variable name
     //denotes that these are private variables that belong to the class itself
     float length_;  //length of pattern in seconds
     float lengthmsec_;
     int nseg_;  //number of segments
     uint dt_;  //delta-t, a.k.a. the time step size in millisec
+    uint odt_; //original delta-t.  Stores a copy in case we modify it
     int nsamp_;
-    int npersegment_;
+    int npersegment_[MAXNSEG];
     int segstarts_[MAXNSEG];
     int segends_[MAXNSEG];
     int isamp_;  //index used for looping through to play the pattern
+    int iseg_; //index used for tracking which segment we are in
+    bool forward_;
+    bool isPaused_;
+    bool doRandom_[MAXNSEG];
+    long randMin_;
+    long randMax_;
     elapsedMillis ticker_; //timer we use for looping
+    elapsedMillis pauseTicker_; //timer that lets us reset to original timer after a hold/pause
     bool isPlaying_;
+    float lastVal_;
+    float lastTime_;
+    float tScale_;
+    float segTscale_[MAXNSEG];
+    byte whichSeg_[MAXNSAMP]; //which segment is a given sample in; has length nsamp_
+    int seglengths_[MAXNSEG];
+    
+    
+    
+
+    bool done_; //flag for tracking if a complete cycle of the pattern has been completed, for once()
+    
+    bool equal_; //flag for whether it's equal-time segments or not
     
 //    int i_;  //this is the index of the time stamp relative to start of pattern
 //    int j_;  //this is the index of the time stamp relative to start of segment
 //    int segment_;
-    float table[TABLELENGTH];
+    float table_[MAXNSAMP];
     
 };
 
